b91ff88af2514a19cfc39d77ed924cee839c000a
Andrew <andrew.daniels@live.com>
2025-03-16 13:06:39 +1000

    Added daily page

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Blueprint,Flask, render_template
-from rolling_days import rolling_days
+from rolling_days import rolling_days, daily
 import sqlite3
 
 app = Flask(__name__)
 
 app.register_blueprint(rolling_days.rolling_days_bp)
+app.register_blueprint(daily.daily_bp)
 #app.register_blueprint(calendar_bp, url_prefix='/calendar')
 
 #Set this to 1 if you need to recreate the table.
-create_moist_db = 1
+create_moist_db = 0
 
 if create_moist_db == 1:
     connection = sqlite3.connect("plant_info.db")
