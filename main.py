8fd803cd4e35b71bbb3a110712526a82d62b52ad
Andrew <andrew.daniels@live.com>
2025-05-01 15:52:38 +1000

    Added get_chart_data

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask
 
-from blueprints.charts import charts
+
 from blueprints import homepage
+from blueprints.charts import charts, get_chart_data
 
 app = Flask(__name__)
 
-app.register_blueprint(charts.charts_bp)
+
 app.register_blueprint(homepage.homepage_bp)
+app.register_blueprint(charts.charts_bp)
+app.register_blueprint(get_chart_data.get_chart_data_bp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
