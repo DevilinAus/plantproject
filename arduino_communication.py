eb8eb5788a3ad0d71360744ed779de7c11602ed6
Andrew <andrew.daniels@live.com>
2025-05-01 16:11:28 +1000

    Updated requirements.txt & Arudino comm to show message at start.

--- arduino_communication.py
+++ arduino_communication.py
@@ -3,14 +3,15 @@ import sqlite3
 import datetime
 import requests
 
-# Define the Ardiuno IP here: NOTE - This is set statically via a static lease on router. 
+# Define the Ardiuno IP here: NOTE - This is set statically via a static lease on router.
 ARDUINO_IP = "192.168.0.99"
+
 
 def get_sensor_data():
 
     # Define the url. Can add query parameters here for cool future features
     url = f"http://{ARDUINO_IP}/"
-    
+
     # Try and connect to the webserver, if response is 200 (good) then get the data.
     try:
         response = requests.get(url)
@@ -26,38 +27,41 @@ def get_sensor_data():
         print(f"Error: {e}")
         return 9999
 
+
 def wait_calculation():
 
-    #Calculate the time since Epoch
+    # Calculate the time since Epoch
     epoch_time = int(time.time())
-    
-    #Calulate how many seconds until the next minute
-    seconds_past_min = (epoch_time%60)
+
+    # Calulate how many seconds until the next minute
+    seconds_past_min = epoch_time % 60
     seconds_remaining_in_min = 60 - seconds_past_min
 
-    #Sleep until the next min
+    # Sleep until the next min
     time.sleep(seconds_remaining_in_min)
-    
-    
+
+
 def main():
-    
+    print("Application Starting!")
     while True:
 
-        #Run function to figure out how long to sleep until the next minute.
+        # Run function to figure out how long to sleep until the next minute.
         wait_calculation()
 
-        #Get the reading from the arduino
+        # Get the reading from the arduino
         time_collected = int(datetime.datetime.now().timestamp())
         print(f" Time Collected: {time_collected}")
         last_generated = get_sensor_data()
 
         connection = sqlite3.connect("plant_info.db")
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO minute_reading (date_time, moisture_reading) VALUES (?, ?)", (time_collected, last_generated))
+        cursor.execute(
+            "INSERT INTO minute_reading (date_time, moisture_reading) VALUES (?, ?)",
+            (time_collected, last_generated),
+        )
         print(f"Wrote to DB -- TIME:{time_collected}, READING:{last_generated}")
-        connection.commit()  
+        connection.commit()
         connection.close()
-    
+
 
 main()
-
