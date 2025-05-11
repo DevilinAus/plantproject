3f86334041cb0b9a8c30b647ed0ba00c2cafa723
Andrew <andrew.daniels@live.com>
2025-05-11 12:36:30 +1000

    Pre-reformatting to unix.

--- app/charts/prepare_data.py
+++ app/charts/prepare_data.py
@@ -12,31 +12,14 @@ def prepare_data(timeframe):
     connection = sqlite3.connect("plant_info.db")
     cursor = connection.cursor()
 
-    # Fetch the last 30 entries of moisture data from the database and assign them to a variable
-
-    # TODO REMOVE THE MINUTE_READING DB AND IF ELSE STATEMENT ONCE MORE DATA AVERAGED OUT
     if timeframe == "1d":
         cursor.execute(
             "SELECT * FROM avg_data ORDER BY date_time DESC LIMIT ?", (data_limit,)
         )
         rows = cursor.fetchall()
-    else:
-        cursor.execute(
-            "SELECT * FROM minute_reading ORDER BY date_time DESC LIMIT ?",
-            (data_limit,),
-        )
-        rows = cursor.fetchall()
-
-    # print(f"Printing rows[1]:{rows[1]}")
 
     # Close the connection
     connection.close()
-
-    # sample_data = {
-    #     "labels": ["2025-05-01 10:00", "2025-05-01 11:00", "2025-05-01 12:00"],
-    #     "values": [30, 35, 28],
-    # }
-    # print(type(max_data_points))
 
     response_data = {
         "labels": [None] * max_data_points[timeframe],
@@ -45,10 +28,6 @@ def prepare_data(timeframe):
 
     now = datetime.now()
     rounded_now = now.replace(minute=0, second=0, microsecond=0)
-    # TODO REMOVE THIS & MODIFY TO HANDLE DIFFERENT TIMEFRAMES
-    #
-    #
-    #
 
     # SET LABELS
     if timeframe == "1d":
@@ -59,17 +38,11 @@ def prepare_data(timeframe):
 
     print(f"Response_data = {response_data}")
 
-    # print(f"Print rows: {rows}")
-
     for i, row in enumerate(rows):
         current_value = row[1]
         datetime_str = row[0]
         datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
         formatted_time_2 = datetime_obj.strftime("%I:%M %p")
-
-        # print(
-        #     f"Formatted Time2= {formatted_time_2}. Response_data label= {response_data['labels'][i]}"
-        # )
 
         for j, label in enumerate(response_data["labels"]):
             print(
