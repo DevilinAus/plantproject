208895551ee78f6d7528e060c530a331e2115e90
Andrew <andrew.daniels@live.com>
2025-05-21 15:12:25 +1000

    Added tests for translate moisture in homepage

---
+++ tests/unit/test_home.py
@@ -0,0 +1,39 @@
+from app.index.home import translate_moisture
+
+
+def describe_translate_moisture():
+    def test_translate_moisture_25():
+        returned_result = translate_moisture(25)
+        assert "Moisture levels critical." in returned_result
+
+    def test_translate_moisture_75():
+        returned_result = translate_moisture(75)
+        assert "Moisture levels balanced." in returned_result
+
+    def test_translate_moisture_125():
+        returned_result = translate_moisture(125)
+        assert "Moisture within acceptable parameters." in returned_result
+
+    def test_translate_moisture_175():
+        returned_result = translate_moisture(175)
+        assert "Moisture decreasing." in returned_result
+
+    def test_translate_moisture_225():
+        returned_result = translate_moisture(225)
+        assert "Warning: Dry conditions detected." in returned_result
+
+    def test_translate_moisture_275():
+        returned_result = translate_moisture(275)
+        assert "Alert: Severe dehydration likely." in returned_result
+
+    def test_translate_moisture_325():
+        returned_result = translate_moisture(325)
+        assert "CRITICAL STATUS!" in returned_result
+
+    def test_translate_moisture_out_of_bounds():
+        returned_result = translate_moisture(9999)
+        assert "Reading outside expected parameters" in returned_result
+
+    def test_translate_moisture_string():
+        returned_result = translate_moisture("ABCD")
+        assert "Non numberic values provided. Consult administrator" in returned_result
