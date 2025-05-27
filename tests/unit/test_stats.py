4102bab3a2b144b19566521b9d3a2e198a123960
Andrew <andrew.daniels@live.com>
2025-05-27 17:48:24 +1000

    Fixed some unused variables

--- tests/unit/test_stats.py
+++ tests/unit/test_stats.py
@@ -1,11 +1,11 @@
 def test_check_heading_rendered(client, mocker):
-    mock_db_fetch = mocker.patch("db.fetch", side_effect=[50, 100, 75])
+    mocker.patch("db.fetch", side_effect=[50, 100, 75])
     response = client.get("/stats")
     assert b"Minimum Moisture Recorded" in response.data
 
 
 def test_check_min_moisture(client, mocker):
-    mock_db_fetch = mocker.patch("db.fetch", side_effect=[50, 100, 75])
+    mocker.patch("db.fetch", side_effect=[50, 100, 75])
     response = client.get("/stats")
     assert b'<td id="min-moisture">50</td>' in response.data
     assert b'<td id="max-moisture">100</td>' in response.data
