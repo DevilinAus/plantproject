01ad21e975db4958bc28ef9174d3d831637e2221
Andrew <andrew.daniels@live.com>
2025-06-19 18:20:34 +1000

    Changes to scripts to use vanilla_db

--- scripts/db/vanilla_db.py
+++ scripts/db/vanilla_db.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-DATABASE_URL = "sqlite:///plant_info.db"
+DATABASE_URI = "sqlite:///instance/plant_info.db"
 
 
-def get_engine_and_session(database_url=DATABASE_URL):
-    engine = create_engine(database_url, echo=False)
+def get_engine_and_session(database_uri=DATABASE_URI):
+    engine = create_engine(database_uri, echo=False, future=True)
     SessionLocal = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
     return engine, SessionLocal
