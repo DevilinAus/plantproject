9d57900b63f2bdef33f6688e0e31f754bd9ab4fc
DevilinAus <112529379+DevilinAus@users.noreply.github.com>
2025-07-01 13:56:12 +1000

    Inital implementation of convolutional neural network for leaf analysis (#26)

--- scripts/plant_disease_training.py
+++ scripts/plant_disease_training.py
@@ -1,5 +1,7 @@
 import os
 import tensorflow as tf
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Input
+from keras.models import Sequential
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -58,7 +60,79 @@ def data_preprocess(training_directory):
 
 
 def main():
+    # This should be adjusted for the total amount of classes(each folder of one classification,
+    # is one class, this will be outputted "Found 20638 files belonging to 15 classes")
+    total_units = 15
+
     train_ds, val_ds = data_preprocess(training_directory)
 
+    model = Sequential()
+    model.add(Input(shape=(256, 256, 3)))
+
+    # TODO: build helper function to cut down the amount of copy pasted code in this next section.
+    # 32
+    model.add(
+        Conv2D(
+            filters=32,
+            kernel_size=3,
+            padding="same",
+            activation="relu",
+        )
+    )
+
+    model.add(Conv2D(filters=32, kernel_size=3, padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=2, strides=2))
+
+    # 64
+    model.add(
+        Conv2D(
+            filters=64,
+            kernel_size=3,
+            padding="same",
+            activation="relu",
+        )
+    )
+
+    model.add(Conv2D(filters=32, kernel_size=3, padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=2, strides=2))
+
+    # 128
+    model.add(
+        Conv2D(
+            filters=128,
+            kernel_size=3,
+            padding="same",
+            activation="relu",
+        )
+    )
+
+    model.add(Conv2D(filters=32, kernel_size=3, padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=2, strides=2))
+
+    # 256
+    model.add(
+        Conv2D(
+            filters=256,
+            kernel_size=3,
+            padding="same",
+            activation="relu",
+        )
+    )
+
+    model.add(Conv2D(filters=32, kernel_size=3, padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=2, strides=2))
+
+    model.add(Flatten())
+    model.add(Dense(units=1024, activation="relu"))
+
+    # Output Layer
+    model.add(Dense(units=total_units, activation="softmax"))
+
+    # Compile Model
+    model.compile(
+        optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
+
+    model.summary()
 
 main()
